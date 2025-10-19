6e29cf16209ea0996b8cdf3506d04b96b33981f3
Asher Bernstein-Meachum <asher.meachum@gmail.com>
2025-10-18 22:38:52 -0500

    Update regression tests to test for regressions of current release

--- test.py
+++ test.py
@@ -7,35 +7,37 @@ from ULN2003_Driver import ULN2003
 import random
 import machine
 
-motor = ULN2003([[18,19,20,21]])
+motor = ULN2003(18,19,20,21)
 frequency = machine.freq()
 machine.freq(250000000)
 
 # This function 
 def correct_usage():
-    motor.turn(0, 1, 100)
-    motor.turn(0, -1, 100)
-    motor.turn(0, 1, 100, 0.02)
-    
+    assert motor.turn(1, 100) is None
+    assert motor.turn(-1, 100) is None
+    assert motor.turn(1, 100, 0.02) is None
+
+'''
+# This part has been commented out since current stable handles errors silently.   
 def incorrect_usage():
     try:
-        test_params = [[0,0,100, 0.002],[0,3,100,0.002],[0,1,0,0.002],[0,1,-4,0.002], [1,1,100,0.002],[-3,1,100,0.002]]
+        test_params = [[0,100, 0.002],[3,100,0.002],[1,0,0.002],[1,-4,0.002], [1,100,0.002],[1,100,0.002]]
         for i in range(len(test_params)):
             motor.turn(test_params[i][0],test_params[i][1],test_params[i][2])
             print(f'Filtering test failed: {test_params[i]}')
             break
     except:
         pass
-
+'''
 # TODO: add logic for handling both valid and invalid inputs
 def random_correct_inputs():
     for i in range(10):
-        param = [0,random.choice([-1,1]), random.randint(1,500), random.randint(2,20)/1000]
+        param = [random.choice([-1,1]), random.randint(1,500), random.randint(2,20)/1000]
         print(f'{i}: {param}')
-        motor.turn(param[0],param[1],param[2])
+        assert motor.turn(param[0],param[1],param[2]) is None
+
 try:
     correct_usage()
-    incorrect_usage()
     random_correct_inputs()
     print("All tests succeeded.")
 finally:
